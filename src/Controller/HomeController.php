027f03ff9d30a4759fcf136b1a8fccb8d1c628bf
TheOracle <78816989+doriandbs@users.noreply.github.com>
2023-10-26 08:35:09 +0100

    init

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->render('admin.html.twig');
+        }
+        return $this->render('home/home.html.twig');
+    }
+}
